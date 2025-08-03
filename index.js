ff51c28f0fd84aff37060a4fb42ed8a68ca57213
Clare Lindley <clare.lindley@gmail.com>
2025-08-03 16:40:14 +0100

    Hello World!

---
+++ index.js
@@ -0,0 +1,43 @@
+// get URL and API key
+// make POST request with auth
+// Choose model and provide a prompt
+// make it take input via cli
+
+// Document steps and rationale
+// fetch over Open AI's client or another lib like axios
+//  a. keep the number of dependencies down
+//  b. learn the protocol first before abstracting it as makes debugging easier- less of a black box
+//  c. get access to latest and beta features - my instance of chatGPT "said" to me "own the abstraction or the abstraction owns you"- and I like that
+// we are in the business of UNDERSTANDING here not just getting something working so lets roll
+// our sleeves up and dig into the API docs.
+
+// fetch API flashcard
+// async/await rules flashcards SyntaxError: await is only valid in async functions and the top level bodies of modules
+import "dotenv/config.js";
+const API_URL = "https://api.openai.com/v1/chat/completions";
+
+async function getAResponse() {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "user",
+          content: "Write a one-sentence bedtime story about a unicorn.",
+        },
+      ],
+      max_tokens: 100,
+    }),
+    headers: {
+      Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const result = await response.json();
+
+  console.log(result.choices[0].message.content);
+}
+
+getAResponse();
